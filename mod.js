868360ddb9465e2e8913d9c37a78fc57c3c286ef
rou <rou.mak@gmail.com>
2021-04-02 19:51:04 +0900

    fix: imports

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
-import { handleRequest } from "~/request_handler.js";
+import { handleRequest } from "./request_handler.js";
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
